b41f77135e20ab3de12166be7640046ecf482acb
bertuccii <fe.bertuccii@gmail.com>
2025-06-02 17:02:14 -0300

    fixing

--- tests/test_auth_service.py
+++ tests/test_auth_service.py
@@ -2,8 +2,9 @@ from app.services.auth_service import register_user, authenticate_user
 from unittest.mock import patch
 
 @patch("app.services.auth_service.User")
-def test_register_user_success(MockUser):
-    db_mock = patch("app.services.auth_service.db").start()
-    token = register_user(username= "test", email= "t@example.com", password= "pass123")
-    assert isinstance(token, str)
+def test_register_user_success():
+    response, status_code = register_user('newuser', 'new@example.com', 'securepass')
+    assert status_code == 201
+    assert isinstance(response, dict)
+    assert response['msg'] == 'User registered successfully'
 
