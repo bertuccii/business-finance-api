35b3ded474343da957f57b5aa82002dcbef4449a
bertuccii <fe.bertuccii@gmail.com>
2025-06-02 17:15:53 -0300

    Update test_auth_service.py

--- tests/test_auth_service.py
+++ tests/test_auth_service.py
@@ -4,4 +4,4 @@ def test_register_user_success(app):
     with app.app_context():
         response, status_code = register_user('newuser', 'new@example.com', 'securepass')
         assert status_code == 201
-        assert response['msg'] == 'User registered successfully'
+        assert response['msg'] == 'User created successfully'
