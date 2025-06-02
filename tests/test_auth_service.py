eb88e78d44bcaeea4390bb9da67b89c152cd1e4e
bertuccii <fe.bertuccii@gmail.com>
2025-06-02 17:06:01 -0300

    Update test_auth_service.py

--- tests/test_auth_service.py
+++ tests/test_auth_service.py
@@ -2,7 +2,7 @@ from app.services.auth_service import register_user, authenticate_user
 from unittest.mock import patch
 
 @patch("app.services.auth_service.User")
-def test_register_user_success():
+def test_register_user_success(mock_user):
     response, status_code = register_user('newuser', 'new@example.com', 'securepass')
     assert status_code == 201
     assert isinstance(response, dict)
